b243b56251206a60aaa35c3b239151c0218e1055
Rumen <rumen_pavlov35@abv.bg>
2021-12-03 12:54:14 +0200

    База

--- KursovProekt1/Form1.Designer.cs
+++ KursovProekt1/Form1.Designer.cs
@@ -28,20 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.опцииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.въвежданеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.колаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.поръчкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.опцииToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(411, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // опцииToolStripMenuItem
+            // 
+            this.опцииToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.въвежданеToolStripMenuItem,
+            this.справкиToolStripMenuItem,
+            this.изходToolStripMenuItem});
+            this.опцииToolStripMenuItem.Name = "опцииToolStripMenuItem";
+            this.опцииToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.опцииToolStripMenuItem.Text = "Опции";
+            // 
+            // въвежданеToolStripMenuItem
+            // 
+            this.въвежданеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.колаToolStripMenuItem,
+            this.поръчкаToolStripMenuItem});
+            this.въвежданеToolStripMenuItem.Name = "въвежданеToolStripMenuItem";
+            this.въвежданеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.въвежданеToolStripMenuItem.Text = "Въвеждане";
+            // 
+            // справкиToolStripMenuItem
+            // 
+            this.справкиToolStripMenuItem.Name = "справкиToolStripMenuItem";
+            this.справкиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.справкиToolStripMenuItem.Text = "Справки";
+            // 
+            // изходToolStripMenuItem
+            // 
+            this.изходToolStripMenuItem.Name = "изходToolStripMenuItem";
+            this.изходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.изходToolStripMenuItem.Text = "Изход";
+            // 
+            // колаToolStripMenuItem
+            // 
+            this.колаToolStripMenuItem.Name = "колаToolStripMenuItem";
+            this.колаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.колаToolStripMenuItem.Text = "Такси";
+            this.колаToolStripMenuItem.Click += new System.EventHandler(this.колаToolStripMenuItem_Click);
+            // 
+            // поръчкаToolStripMenuItem
+            // 
+            this.поръчкаToolStripMenuItem.Name = "поръчкаToolStripMenuItem";
+            this.поръчкаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.поръчкаToolStripMenuItem.Text = "Поръчка";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 47);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 94);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(387, 210);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Търси";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 380);
+            this.ClientSize = new System.Drawing.Size(411, 332);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ТаксиМакси";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem опцииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem въвежданеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem колаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem изходToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem поръчкаToolStripMenuItem;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- KursovProekt1/Form1.cs
+++ KursovProekt1/Form1.cs
@@ -16,5 +16,6 @@ namespace KursovProekt1
         {
             InitializeComponent();
         }
+
     }
 }
